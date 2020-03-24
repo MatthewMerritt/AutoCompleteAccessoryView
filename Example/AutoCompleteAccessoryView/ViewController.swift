5312f7a5e90a50a1516f479610bb5baf5d359c64
Matthew Merritt <matthew.merritt@yahoo.com>
2020-03-24 07:46:32 -0700

    Initial Push

--- Example/AutoCompleteAccessoryView/ViewController.swift
+++ Example/AutoCompleteAccessoryView/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AutoCompleteAccessoryView
 
 class ViewController: UIViewController {
 
@@ -20,5 +21,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionButtonAction(_ sender: UIButton) {
+
+        let ac = UIAlertController(title: "StrengthRep Exercise", message: nil, preferredStyle: .alert)
+
+        ac.addTextField { textField in
+
+            textField.placeholder = "Enter Name"
+            textField.autocapitalizationType = .words
+            textField.clearButtonMode = .whileEditing
+
+            let autoCompleteView = AutoCompleteAccessoryView()
+
+            autoCompleteView.setup()
+            autoCompleteView.searchStrings = ["Apple", "Apple Sauce", "Pineapple", "Orange", "MoFu"]
+            autoCompleteView.textField = textField
+
+            textField.inputAccessoryView = autoCompleteView
+
+            textField.on.textChange { string in
+                autoCompleteView.typed = string
+            }
+
+        }
+
+        ac.addTextField { textField in
+
+            textField.placeholder = "Enter Description"
+            textField.autocapitalizationType = .words
+            textField.clearButtonMode = .whileEditing
+
+            let autoCompleteView = AutoCompleteAccessoryView()
+
+            autoCompleteView.setup()
+            autoCompleteView.searchStrings = ["iPhone", "iPad", "iPod", "AirPod", "iMac", "HomePod"]
+            autoCompleteView.textField = textField
+
+            textField.inputAccessoryView = autoCompleteView
+
+            textField.on.textChange { string in
+                autoCompleteView.typed = string
+            }
+
+
+
+        }
+
+
+        ac.message = "Add this CardioRep Name"
+
+        let submitAction = UIAlertAction(title: "Add", style: .default) { [unowned ac] _ in
+            let nameField = ac.textFields![0]
+            let descriptionField = ac.textFields![1]
+            // do something interesting with "answer" here
+
+            print("nameField: \(nameField.text!)")
+            print("descriptionField: \(descriptionField.text!)")
+
+            self.dismiss(animated: true) {}
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in
+            self.dismiss(animated: true) {}
+        }
+
+        ac.addAction(cancelAction)
+        ac.addAction(submitAction)
+
+        DispatchQueue.main.async {
+            self.present(ac, animated: true) { }
+        }
+
+    }
 }
 
