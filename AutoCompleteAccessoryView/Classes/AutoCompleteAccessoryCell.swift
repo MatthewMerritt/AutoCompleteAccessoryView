d9734ae62be2956f1560564246fc19911de1adba
Matthew Merritt <matthew.merritt@yahoo.com>
2020-03-24 07:52:02 -0700

    Added Classes

---
+++ AutoCompleteAccessoryView/Classes/AutoCompleteAccessoryCell.swift
@@ -0,0 +1,76 @@
+//
+//  AutoCompleteCell.swift
+//  LineChartExample
+//
+//  Created by Matthew Merritt on 3/23/20.
+//  Copyright © 2020 Matthew Merritt. All rights reserved.
+//
+
+import UIKit
+import EasyClosure
+
+public class AutoCompleteAccessoryCell: UICollectionViewCell {
+
+    public weak var autoCompletView: AutoCompleteAccessoryView?
+
+    public let label: UILabel = {
+        let label = UILabel()
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.textAlignment = .left
+
+        return label
+    }()
+
+    public let seperator: UIView = {
+        let lineView = UIView(frame: .zero)
+
+        lineView.backgroundColor = .gray
+
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+
+        return lineView
+
+    }()
+
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(label)
+
+        label.textColor = .link
+
+        label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+
+        addSubview(seperator)
+
+        seperator.widthAnchor.constraint(equalToConstant: 1).isActive = true
+        seperator.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 3).isActive = true
+        seperator.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
+        seperator.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12).isActive = true
+
+    }
+
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override var isHighlighted: Bool {
+        didSet {
+            if self.isHighlighted {
+                // Your customized animation or add a overlay view
+                label.textColor = .darkText
+                autoCompletView?.isScrollEnabled = false
+
+            } else {
+                // Your customized animation or remove overlay view
+                label.textColor = .link
+                autoCompletView?.isScrollEnabled = true
+            }
+        }
+    }
+
+}
---
+++ AutoCompleteAccessoryView/Classes/AutoCompleteAccessoryView.swift
@@ -0,0 +1,108 @@
+//
+//  AutoCompleteView.swift
+//  LineChartExample
+//
+//  Created by Matthew Merritt on 3/23/20.
+//  Copyright © 2020 Matthew Merritt. All rights reserved.
+//
+
+import UIKit
+import EasyClosure
+
+public class AutoCompleteAccessoryView: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "CellId1"
+
+    public var searchStrings: [String] = []
+
+    private var displayStrings: [String] {
+        get {
+            return self.typed == "" ? searchStrings : self.searchStrings.filter { $0.range(of: self.typed, options: .caseInsensitive) != nil }
+        }
+    }
+
+    public var typed: String = "" {
+        didSet {
+            performBatchUpdates({
+                reloadSections(IndexSet(arrayLiteral: 0))
+            }, completion: nil)
+        }
+    }
+
+    public var textField: UITextField?
+
+    public convenience init(collectionViewLayout layout: UICollectionViewLayout) {
+        self.init(frame: CGRect.zero, collectionViewLayout: layout)
+    }
+
+    public convenience init() {
+        self.init(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+    }
+
+    public func setup() {
+
+        let blurEffect = UIBlurEffect(style: .prominent)
+
+        let blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        blurredEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurredEffectView.frame = bounds
+
+        backgroundColor = .clear
+        backgroundView = blurredEffectView
+
+        autoresizingMask = [.flexibleWidth]
+
+        register(AutoCompleteAccessoryCell.self, forCellWithReuseIdentifier: cellId)
+        (collectionViewLayout as! UICollectionViewFlowLayout).scrollDirection = .horizontal
+
+        delegate = self
+        dataSource = self
+
+        frame.size.height = 44
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return displayStrings.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            // dataArary is the managing array for your UICollectionView.
+
+        let item = displayStrings[indexPath.row]
+        var itemSize = item.size(withAttributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17)
+        ])
+        itemSize.height = 44
+        itemSize.width += 20
+
+        return itemSize
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AutoCompleteAccessoryCell
+
+        cell.backgroundColor = .clear
+        cell.label.text = displayStrings[indexPath.row]
+
+        cell.seperator.backgroundColor = indexPath.row == displayStrings.count - 1 ? .clear : .lightGray
+        cell.autoCompletView = self
+        return cell
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let text = (collectionView.cellForItem(at: indexPath) as! AutoCompleteAccessoryCell).label.text {
+            textField?.text = text
+            typed = text
+        }
+    }
+
+}
